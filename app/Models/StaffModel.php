6a6c0f4eef2676189361234892c52817ca3aefcb
dmjrmh <gban1mj27@gmail.com>
2025-11-02 15:50:44 +0700

    added some models

---
+++ app/Models/StaffModel.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class StaffModel extends Model
+{
+    protected $table            = 'staffs';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $insertID         = 0;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = true;
+    protected $protectFields    = true;
+    protected $allowedFields    = [
+      'store_id',
+      'staff_code',
+      'name',
+      'position',
+      'phone',
+      'is_active',
+    ];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+}
---
+++ app/Models/StoreModel.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class StoreModel extends Model
+{
+    protected $table            = 'stores';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $insertID         = 0;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = true;
+    protected $protectFields    = true;
+    protected $allowedFields    = [
+      'store_name',
+      'store_code',
+      'address',
+      'city',
+      'is_active'
+    ];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+}
